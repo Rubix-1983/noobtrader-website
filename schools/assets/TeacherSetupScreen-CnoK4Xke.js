e4c0399b56c982884c1e3d528615a2af8ce5ccf3
Rubix-1983 <132808079+Rubix-1983@users.noreply.github.com>
2026-08-11 08:51:26 +0200

    Deploy web app from Noob-Trader@6bce5d1

--- schools/assets/TeacherSetupScreen-CnoK4Xke.js
+++ schools/assets/TeacherSetupScreen-CnoK4Xke.js
@@ -1,4 +1,4 @@
-import{e as U,s as st,l as ce,r as l,f as j,j as t,g as lt,h as Re,i as ct,k as dt,T as ht,m as de,X as Ne,n as ut,o as mt,p as ft,q as gt,t as yt,v as pt,w as _t,x as xt,y as kt,z as vt,D as De,F as bt,G as jt}from"./index-tINhcdn9.js";import{c as Te,D as Ie,s as St,M as wt,a as Ct,V as zt}from"./teacherVerification-COdln2ID.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";/**
+import{e as U,s as st,l as ce,r as l,f as j,j as t,g as lt,h as Re,i as ct,k as dt,T as ht,m as de,X as Ne,n as ut,o as mt,p as ft,q as gt,t as yt,v as pt,w as _t,x as xt,y as kt,z as vt,D as De,F as bt,G as jt}from"./index-DJXpqYTJ.js";import{c as Te,D as Ie,s as St,M as wt,a as Ct,V as zt}from"./teacherVerification-Ccxu-1ne.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
