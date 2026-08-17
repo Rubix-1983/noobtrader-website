15162eef47dbc0f893b84d774a1999e5292e9acd
Rubix-1983 <132808079+Rubix-1983@users.noreply.github.com>
2026-08-17 11:58:44 +0200

    Deploy web app from Noob-Trader@1ca56aa (+uncommitted changes)

---
+++ schools/assets/index-DZ6rxEJK.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Bvg9h5m2.js","./index-KA4-vV8J.js","./react-CRB3T2We.js","./supabase-BiHzSKx7.js","./index-OTriKTl0.css"])))=>i.map(i=>d[i]);
+import{$ as r,_ as t}from"./index-KA4-vV8J.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";const _=r("App",{web:()=>t(()=>import("./web-Bvg9h5m2.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url).then(p=>new p.AppWeb)});export{_ as App};
