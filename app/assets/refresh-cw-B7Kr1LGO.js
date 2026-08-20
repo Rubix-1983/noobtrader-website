4fff168a8428145bfadbc3f87cd1928002e84860
Rubix-1983 <132808079+Rubix-1983@users.noreply.github.com>
2026-08-20 16:16:39 +0200

    Deploy web app from Noob-Trader@c2649c7 (+uncommitted changes)

--- app/assets/refresh-cw-B7Kr1LGO.js
+++ app/assets/refresh-cw-B7Kr1LGO.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DhnmGLI4.js";/**
+import{c as e}from"./index-B8duKeiP.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
