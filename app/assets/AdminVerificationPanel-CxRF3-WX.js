e305c0660d376afbc251307d81585715b5752d39
Rubix-1983 <132808079+Rubix-1983@users.noreply.github.com>
2026-08-20 10:17:38 +0200

    Deploy web app from Noob-Trader@3f577c4 (+uncommitted changes)

--- app/assets/AdminVerificationPanel-CxRF3-WX.js
+++ app/assets/AdminVerificationPanel-CxRF3-WX.js
@@ -1,4 +1,4 @@
-import{c as W,r as o,bC as q,j as n,X as O,bD as V,bE as P,u as E}from"./index-DdlugFeO.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";/**
+import{c as W,r as o,bC as q,j as n,X as O,bD as V,bE as P,u as E}from"./index-BYLe47vI.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
