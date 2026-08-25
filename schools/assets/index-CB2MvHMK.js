ce36a2af54df11637a763037a92f1eafd4ec598b
Rubix-1983 <132808079+Rubix-1983@users.noreply.github.com>
2026-08-25 16:29:32 +0200

    Deploy web app from Noob-Trader@6c3ec4d (+uncommitted changes)

--- schools/assets/index-CB2MvHMK.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BIXmRLBY.js","./index-CJ0QHu1s.js","./react-CRB3T2We.js","./supabase-BiHzSKx7.js","./index-OTriKTl0.css"])))=>i.map(i=>d[i]);
-import{ad as r,$ as t}from"./index-CJ0QHu1s.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";const m=r("App",{web:()=>t(()=>import("./web-BIXmRLBY.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url).then(p=>new p.AppWeb)});export{m as App};
---
+++ schools/assets/index-Cfmso-VM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DvrvKDKI.js","./index-CpLSesao.js","./react-CRB3T2We.js","./supabase-BiHzSKx7.js","./index-OTriKTl0.css"])))=>i.map(i=>d[i]);
+import{ab as r,_ as t}from"./index-CpLSesao.js";import"./react-CRB3T2We.js";import"./supabase-BiHzSKx7.js";const _=r("App",{web:()=>t(()=>import("./web-DvrvKDKI.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url).then(p=>new p.AppWeb)});export{_ as App};
